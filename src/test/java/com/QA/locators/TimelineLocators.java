e405ac1aef95cefa8ce59ac473ac73ccbbd2113b
ygtari <yassine.gtari@advyteam.com>
2019-10-01 16:54:16 +0100

    Adding ModificationStatut and SuppressionStatut features

--- src/test/java/com/QA/locators/TimelineLocators.java
+++ src/test/java/com/QA/locators/TimelineLocators.java
@@ -2,13 +2,17 @@ package com.QA.locators;
 
 public class TimelineLocators {
 
-public static final String Champ_Statut = "publish-clickId";
-public static final String Champ_Input_Statut = "//*[@id=\"timeline\"]/acc-timeline-publish/div/div[1]/acc-mention-per-timelines/quill-editor/div/div[1]";
-public static final String Liste_Deroulante_Timeline = "//*[@id=\"timeline\"]/acc-timeline-publish/div/div[2]/div/ng-select/div/div[2]/span";
-public static final String Premiere_Timeline_liste = "//*[@id=\"timeline\"]/acc-timeline-publish/div/div[2]/div/ng-select/div/ul/li[2]/div/a/div";
-public static final String Bouton_Publication_Statut = "//*[@id=\"timeline\"]/acc-timeline-publish/div/div[2]/button";
-public static final String Premiere_Publication_Timeline = "//*[@id=\\\"post-holder\\\"]/div[3]";
-
-
+    public static final String Champ_Statut = "publish-clickId";
+    public static final String Champ_Input_Statut = "//*[@id=\"timeline\"]/acc-timeline-publish/div/div[1]/acc-mention-per-timelines/quill-editor/div/div[1]";
+    public static final String Liste_Deroulante_Timeline = "//*[@id=\"timeline\"]/acc-timeline-publish/div/div[2]/div/ng-select/div/div[2]/span";
+    public static final String Premiere_Timeline_liste = "//*[@id=\"timeline\"]/acc-timeline-publish/div/div[2]/div/ng-select/div/ul/li[2]/div/a/div";
+    public static final String Bouton_Publication_Statut = "//*[@id=\"timeline\"]/acc-timeline-publish/div/div[2]/button";
+    public static final String Premiere_Publication_Timeline = "//*[@id=\"post-holder\"]/div[3]";
+    public static final String Bouton_Menu_Modif_Supp_Statut = "//*[@id=\"post-holder\"]/div[2]/i" ;
+    public static final String Bouton_Modif_Statut ="//*[@id=\"post-holder\"]/div[2]/ul/li[1]/a";
+    public static final String Champ_Input_Modification_Statut ="/html/body/ngb-modal-window/div/div/acc-edit-post-modal/div/div[1]/fieldset/acc-mention-per-timelines/quill-editor/div/div[1]";
+    public static final String  Bouton_Publier_Statut="/html/body/ngb-modal-window/div/div/acc-edit-post-modal/div/div[1]/fieldset/button[1]";
+    public static final String  Bouton_Supprimer_Statut="//*[@id=\"post-holder\"]/div[2]/ul/li[2]/a";
+    public static final String  Bouton_Valider_Suppression_Statut="/html/body/modal-container/div/div/div/div[3]/button[2]";
 
 }
