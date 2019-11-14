2ab418bb398d21bcd522cac396fd60bc85bab4af
ygtari <yassine.gtari@advyteam.com>
2019-11-14 09:50:27 +0100

    Updating WorkflowDemandeManagerValidee Steps defention.class Updating RecrutemntLocators.class Updating CommonLocators.class

--- src/test/java/com/QA/locators/RecrutementLocators.java
+++ src/test/java/com/QA/locators/RecrutementLocators.java
@@ -1,4 +1,7 @@
 package com.QA.locators;
 
 public class RecrutementLocators {
+
+    public static final String Icone_Demande_Recrutement ="/html/body/app-root/app-layout/div/section/div/acc-global-container/acc-portal-container/div/div/div[1]/acc-left-side-container/div/acc-user-widget/div/div[3]/div/div[2]/ul/li[4]/a";
+
 }
--- src/test/java/com/QA/steps/Talent/Recrutement/WorkflowDemandeManagerValidee.java
+++ src/test/java/com/QA/steps/Talent/Recrutement/WorkflowDemandeManagerValidee.java
@@ -1,7 +1,11 @@
 package com.QA.steps.Talent.Recrutement;
 
+import com.QA.locators.CommonLocators;
+import com.QA.locators.RecrutementLocators;
+import com.QA.steps.ActionsCommunes;
 import com.QA.steps.GenerateurDriver;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -10,14 +14,31 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class WorkflowDemandeManagerValidee {
 
     private final WebDriver driver = GenerateurDriver.driver;
+    private ActionsCommunes action = new ActionsCommunes();
 
     @Then("le nom de l utilisateur {string} est affiche en haut a gauche")
     public void leNomDeLUtilisateurEstAfficheEnHautAGauche(String arg0) {
 
         boolean myDynamicElement = (new WebDriverWait(driver,60))
-                .until(ExpectedConditions.attributeContains(By.xpath("/html/body/app-root/app-layout/div/section/div/acc-global-container/acc-portal-container/div/div/div[1]/acc-left-side-container/div/acc-user-widget/div/div[2]/span/span"),"innerText", "Yasushi TAKAGI"));
+                .until(ExpectedConditions.attributeContains(By.xpath(CommonLocators.Icone_Nom_Utilisateur),"innerText", arg0));
          
     }
+
+
+    @When("l utlisateur clique sur l icone mon equipe")
+    public void lUtlisateurCliqueSurLIconeMonEquipe() {
+
+        driver.findElement(By.xpath(CommonLocators.Icone_MonEquipe)).click();
+    }
+
+
+    @When("l utilisateur clique sur l icone demande de recrutement")
+    public void lUtilisateurCliqueSurLIconeDemandeDeRecrutement() throws InterruptedException {
+
+        driver.findElement(By.xpath(RecrutementLocators.Icone_Demande_Recrutement)).click();
+        action.pause(driver,3000);
+    }
+
 
 
 }
