c0ef96c54d00f05d15f6ef5baa3111ee4163439d
ygtari <yassine.gtari@advyteam.com>
2020-03-26 19:23:21 +0100

    Updating LiensUtilesLocators.java

--- src/test/java/com/QA/locators/LiensUtilesLocators.java
+++ src/test/java/com/QA/locators/LiensUtilesLocators.java
@@ -6,7 +6,7 @@ public class LiensUtilesLocators {
   public static final String Bouton_Ajouter_Lien="button[class='btn pull-right square-widget color-follow-bg']";
   public static final String Titre_Lien_Creation="input[formcontrolname='label']";
   public static final String Champ_Lien_Creation="input[formcontrolname='link']";
-  public static final String Description_Lien_Creation="textarea[formcontrolname='description']";
+  public static final String Description_Lien_Creation="/html/body/ngb-modal-window/div/div/acc-useful-link-form/form/div/div[2]/section/div[1]/div[1]/div[3]/textarea";
   public static final String Bouton_Confirmer_Ajout_Lien="//*[@id=\"popup-add-useful-information\"]/div[2]/div/button[2]";
   public static final String Premier_Lien_Afficher_Titre="/html/body/app-root/app-layout/div/section/div/acc-useful-link-container/div[2]/acc-useful-link-list/div[1]/acc-useful-link-card/div/div/div/div[1]/h4";
   public static final String Premier_Lien_Afficher_LienUtile="/html/body/app-root/app-layout/div/section/div/acc-useful-link-container/div[2]/acc-useful-link-list/div[1]/acc-useful-link-card/div/div/div/div[2]/h5/a";
