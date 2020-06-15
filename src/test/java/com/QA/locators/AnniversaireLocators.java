8e690852a8fa4113fc9dca38a5a7e11705d1b035
ygtari <yassine.gtari@advyteam.com>
2020-06-15 16:20:41 +0100

    Updating Anniversaire_Professionnel_Profil_Enrichi xpath

--- src/test/java/com/QA/locators/AnniversaireLocators.java
+++ src/test/java/com/QA/locators/AnniversaireLocators.java
@@ -8,7 +8,7 @@ public class AnniversaireLocators {
   public static final String JS_Bouton_Collaborateur_Anniversaire_Personnel="/html/body/app-root/app-layout/div/app-offsidebar/div/div[2]/div/div[3]/acc-profile-configuration/div/div[3]/div[2]/div/acc-switch/div/label/input";
   public static final String Anniversaire_Personnel_Profil_Enrichi="i[title=\"Date d'anniversaire\"]";
   public static final String JS_Check_Box_AnniversaireProfessionnel_Rh="/html/body/app-root/app-layout/div/section/div/acc-core-config-container/div/div[2]/echelon-config/div/div[2]/div/form/div/table/tbody/tr[10]/td[2]/acc-switch[1]/div/label/input";
-  public static final String Anniversaire_Professionnel_Profil_Enrichi="/html/body/app-root/app-layout/div/section/div/ng-component/div/acc-connect-rich-profile-container/div/div[1]/acc-connect-about-summary/div[1]/div[2]/ul/li[6]/span";
+  public static final String Anniversaire_Professionnel_Profil_Enrichi="/html/body/app-root/app-layout/div/section/div/ng-component/div/acc-connect-rich-profile-container/div/div[1]/acc-connect-about-summary/div[1]/div/ul/li[5]/span";
   public static final String Check_Box_AnniversaireProfessionnel_Rh_Click="//*[@id=\"configTable\"]/tbody/tr[9]/td[2]/div/label/span/span";
   public static final String Bouton_Anniversaire_Portal="/html/body/app-root/app-layout/div/section/div/acc-global-container/acc-portal-container/div/div/div[3]/acc-right-side-container/div/div[4]/acc-right-side-tabs/ul/li[3]/a";
   public static final String Liste_Anniversaire="a[class*='media p mt0 list-group-item']";
