477a277f634b00e2d33751a68a46ff52f24f89c1
ygtari <yassine.gtari@advyteam.com>
2020-06-23 13:57:33 +0100

    Updating AjoutDunAxeSansLibelle.java

--- src/test/java/com/QA/steps/talent/configuration/AjoutDunAxeSansLibelle.java
+++ src/test/java/com/QA/steps/talent/configuration/AjoutDunAxeSansLibelle.java
@@ -10,13 +10,12 @@ import org.openqa.selenium.WebDriver;
 public class AjoutDunAxeSansLibelle {
 
     private static final WebDriver driver = GenerateurDriver.driver;
-    private static final String border="1px solid rgb[(240, 0, 51])";
+    private static final String border="1px solid rgb(240, 0, 51)";
 
     @Then("verifier que le champs libelle est encadre en rouge")
     public void verifierQueLeChampsLibelleEstEncadreEnRouge() {
 
         String actualBorder =driver.findElement(By.xpath(ConfigurationTalentLocators.Libelle_Ajout_Axe)).getCssValue("border");
-
         Assert.assertEquals(border,actualBorder);
 
     }
